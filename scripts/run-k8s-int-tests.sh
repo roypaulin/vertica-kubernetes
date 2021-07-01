4ba5c576d38215ab516aea2c4b48b32df51604db
roypaulin <55731470+roypaulin@users.noreply.github.com>
2021-07-01 12:21:27 +0200

    Update run-k8s-int-tests.sh

--- scripts/run-k8s-int-tests.sh
+++ scripts/run-k8s-int-tests.sh
@@ -103,7 +103,7 @@ function build_and_push {
 # Run integration tests and store the pod status in a file
 function run_integration_tests {
   echo "Saving the test status log in $INT_TEST_OUTPUT_DIR/integration_run.log "
-  make run-int-tests
+  kubectl kuttl test --test auto-restart-vertica
 }
 
 trap cleanup EXIT
